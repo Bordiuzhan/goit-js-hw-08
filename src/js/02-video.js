81dfeabee6a1d27725cb4a2f042f37d11a7254bd
Bordiuzhan <bordyujan@gmail.com>
2022-10-02 18:50:15 +0200

    task 2 add JSON

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
-const timeRecovery = localStorage.getItem('videoplayer-current-time');
+const timeRecovery = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
 
 player.on('timeupdate', throttle(onPlayTime, 1000));
 player.setCurrentTime(timeRecovery);
 
 function onPlayTime(e) {
-  const currentTime = e.seconds;
+  const currentTime = JSON.stringify(e.seconds);
   localStorage.setItem('videoplayer-current-time', currentTime);
 }
