5c585adbd4302c13d5056fa1937d4cb6d815ad6e
Bordiuzhan <bordyujan@gmail.com>
2022-10-02 14:07:45 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', onForm);
+
+const setValue = {
+  email: 'ret',
+  message: '',
+};
+form[0].textContent = setValue.email;
+
+console.log(form[0].textContent);
+function onForm(e) {
+  if (e.target.name === 'email') {
+    setValue.email = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(setValue));
+  }
+  if (e.target.name === 'message') {
+    setValue.message = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(setValue));
+  }
+}
